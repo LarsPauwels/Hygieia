5f57fa67828e0c4bdd35c4dd3d7fc228e2e81b39
RobbeCl <php.robbe.claessens@gmail.com>
2020-09-11 15:10:24 +0200

    cleanup

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Hygieia',
             'email' => 'info@hygieia.be',
+            'role_id' => 1,
             'password' => bcrypt('HygieiaPassword'),
         ]);
     }
